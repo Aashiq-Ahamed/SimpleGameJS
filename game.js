5de0eb6904c070f7197bf52a0df5b3ef17335eb2
aashiq.a <aashiq.ahamed@scicom.com.my>
2024-07-23 11:34:21 +0530

    test

--- game.js
+++ game.js
@@ -6,7 +6,9 @@ canvas.height = 600;
 let gameInterval;
 let player;
 let platforms;
-let currentLevel = 1;
+let score;
+let scoreInterval;
+let platformGenerationInterval;
 
 class Player {
     constructor(x, y, width, height, color) {
@@ -40,12 +42,24 @@ class Player {
         if (this.x < 0) this.x = 0;
         if (this.x + this.width > canvas.width) this.x = canvas.width - this.width;
         if (this.y + this.height > canvas.height) this.y = canvas.height - this.height;
+
+        // Check if player is grounded
+        this.isGrounded = false;
+        for (let platform of platforms) {
+            if (this.x < platform.x + platform.width &&
+                this.x + this.width > platform.x &&
+                this.y + this.height > platform.y &&
+                this.y + this.height < platform.y + platform.height) {
+                this.isGrounded = true;
+                this.velocityY = 0;
+                this.y = platform.y - this.height;
+                break;
+            }
+        }
     }
 
     jump() {
-        console.log('here')
-        if (true) {
-            console.log('Now here')
+        if (this.isGrounded) {
             this.velocityY = -this.jumpForce;
             this.isGrounded = false;
         }
@@ -53,47 +67,64 @@ class Player {
 }
 
 class Platform {
-    constructor(x, y, width, height, color) {
+    constructor(x, y, width, height, color, speed) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.color = color;
+        this.speed = speed;
     }
 
     draw() {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-}
 
-function createLevel(level) {
-    if (level === 1) {
-        return [
-            new Platform(0, 580, 800, 20, 'green'),
-            new Platform(100, 450, 200, 20, 'green'),
-            new Platform(400, 350, 200, 20, 'green'),
-        ];
-    } else if (level === 2) {
-        return [
-            new Platform(0, 580, 800, 20, 'green'),
-            new Platform(50, 450, 150, 20, 'green'),
-            new Platform(300, 300, 250, 20, 'green'),
-            new Platform(600, 200, 150, 20, 'green'),
-        ];
+    update() {
+        this.y += this.speed;
+        if (this.y > canvas.height) {
+            this.removePlatform();
+        }
+    }
+
+    removePlatform() {
+        const index = platforms.indexOf(this);
+        if (index > -1) {
+            platforms.splice(index, 1);
+        }
     }
 }
 
-function startGame() {
-    player = new Player(50, 550, 30, 30, 'blue');
-    platforms = createLevel(currentLevel);
-    gameInterval = setInterval(updateGame, 20);
+function generatePlatform() {
+    const width = Math.random() * 200 + 100; // Random width between 100 and 300
+    const x = Math.random() * (canvas.width - width); // Random x position within canvas
+    const platform = new Platform(x, 0, width, 20, 'green', 1);
+    platforms.push(platform);
 }
 
-function selectLevel(level) {
+function startGame() {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('gameOver').style.display = 'none';
+    score = 0;
+    document.getElementById('score').innerText = 'Score: 0';
+    player = new Player(50, 550, 30, 30, 'blue');
+    platforms = [new Platform(0, 580, 800, 20, 'green', 0)];
+    gameInterval = setInterval(updateGame, 20);
+    scoreInterval = setInterval(updateScore, 1000);
+    platformGenerationInterval = setInterval(generatePlatform, 3000); // Generate a new platform every 3 seconds
+}
+
+function updateScore() {
+    score++;
+    document.getElementById('score').innerText = 'Score: ' + score;
+}
+
+function gameOver() {
     clearInterval(gameInterval);
-    currentLevel = level;
-    startGame();
+    clearInterval(scoreInterval);
+    clearInterval(platformGenerationInterval);
+    document.getElementById('gameOver').style.display = 'block';
 }
 
 function updateGame() {
@@ -103,24 +134,17 @@ function updateGame() {
     player.draw();
 
     platforms.forEach(platform => {
+        platform.update();
         platform.draw();
-
-        // Check for collision with platforms
-        if (player.x < platform.x + platform.width &&
-            player.x + player.width > platform.x &&
-            player.y < platform.y + platform.height &&
-            player.y + player.height > platform.y) {
-            player.isGrounded = true;
-            player.velocityY = 0;
-            player.y = platform.y - player.height;
-        }
     });
 
-    player.isGrounded = player.y + player.height >= canvas.height;
+    // Check if player has fallen off the screen
+    if (player.y + player.height >= canvas.height) {
+        gameOver();
+    }
 }
 
 document.addEventListener('keydown', (e) => {
-    console.log(e)
     if (e.code === 'ArrowLeft') {
         player.velocityX = -player.speed;
     } else if (e.code === 'ArrowRight') {
